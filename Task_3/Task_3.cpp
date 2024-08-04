ad177be732d8624c49820c479685e2e64824a39e
zayts_home <10kcin01@gmail.com>
2024-08-04 14:47:48 +0500

    Task#3 complite

--- Task_3/Task_3.cpp
+++ Task_3/Task_3.cpp
@@ -1,6 +1,26 @@
 ﻿#include <iostream>
+#include <filesystem>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    /*for(auto& p: std::filesystem::recursive_directory_iterator("dev-skillbox-module25\\"))
+    {
+        if (!(bool)(p.path().extension().compare(".h")))
+        {
+            std::cout << p.path() << '\n';
+        }
+    }*/
+    auto recursiveGetFileNamesByExtension =
+        [](std::filesystem::path path = "dev-skillbox-module25\\",
+            const std::string extension = ".h")
+        {
+            for (auto& p : std::filesystem::recursive_directory_iterator(path))
+            {
+                if (!(bool)(p.path().extension().compare(extension)))
+                {
+                    std::cout << p.path() << '\n';
+                }
+            }
+        };
+    recursiveGetFileNamesByExtension();
+
 }
